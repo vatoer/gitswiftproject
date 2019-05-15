6d85059ff58033b43cf026b678d3b4246bc58cdd
vatoer.ckplus@gmail.com <vatoer.ckplus@gmail.com>
2019-05-15 08:15:05 +0700

    Initial Commit

---
+++ gitswiftproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitswiftproject
+//
+//  Created by fathur rohman on 15/05/19.
+//  Copyright © 2019 fathur rohman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
